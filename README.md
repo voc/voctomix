fed9afe824926ef77c1cd815a5a0b41171b7bff4
MaZderMind <github@mazdermind.de>
2014-07-29 10:48:40 +0200

    license foo

---
+++ README.md
@@ -0,0 +1,2 @@
+= Voctomix =
+Building a HD-Compatible, Multi-Track-Audio-Capable Software-Based Live-Video-Mixer for the VOC/FEM.
--- gst-vocschnipselssink/src/gstvocschnipselsink.c
+++ gst-vocschnipselssink/src/gstvocschnipselsink.c
@@ -2,7 +2,7 @@
  * GStreamer
  * Copyright (C) 2005 Thomas Vander Stichele <thomas@apestaart.org>
  * Copyright (C) 2005 Ronald S. Bultje <rbultje@ronald.bitfreak.net>
- * Copyright (C) 2014 Peter Körner <<user@hostname.org>>
+ * Copyright (C) 2014 Peter Körner <peter@mazdermind.de>>
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
