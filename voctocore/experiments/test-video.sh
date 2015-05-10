f8a0b46028b9c902da7ce67590f9ce952ea44544
MaZderMind <github@mazdermind.de>
2015-05-10 11:28:51 +0200

    Fix Caps in Test-Source

--- voctocore/experiments/test-video.sh
+++ voctocore/experiments/test-video.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gst-launch-1.0 videotestsrc ! video/x-raw,height=600,width=800 ! timeoverlay valignment=bottom ! gdppay ! tcpclientsink host=localhost port=5000
+gst-launch-1.0 videotestsrc ! video/x-raw,height=600,width=800,format=I420,framerate=25/1 ! timeoverlay valignment=bottom ! gdppay ! tcpclientsink host=localhost port=5000
