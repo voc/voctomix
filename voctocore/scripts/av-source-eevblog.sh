14830e261e86e34ec249c73c6548119eba9d19da
MaZderMind <github@mazdermind.de>
2015-05-11 23:00:40 +0200

    rename av-source scripts

---
+++ voctocore/scripts/av-source-eevblog.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+gst-launch-1.0 -vm \
+	filesrc location=$HOME/eevblog.mp4 !\
+	decodebin name=src \
+	\
+	src. !\
+	queue !\
+	videoconvert !\
+	videoscale !\
+	video/x-raw,format=I420,width=1280,height=720,framerate=25/1,pixel-aspect-ratio=1/1 ! \
+	timeoverlay valignment=bottom ! \
+	gdppay ! \
+	tcpclientsink host=localhost port=10000 \
+	\
+	src. !\
+	queue !\
+	audioconvert !\
+	audioresample !\
+	audiorate !\
+	audio/x-raw,format=S16LE,channels=2,layout=interleaved,rate=48000 !\
+	gdppay !\
+	tcpclientsink host=localhost port=20000
