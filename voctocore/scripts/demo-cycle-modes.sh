1aad026e2e68bd8da81cae83402c27d929d9b141
MaZderMind <github@mazdermind.de>
2015-05-22 12:20:15 +0200

    add cycle-script to test all composition modes

---
+++ voctocore/scripts/demo-cycle-modes.sh
@@ -0,0 +1,22 @@
+while true; do
+	sleep 10
+	./set-composite-side-by-side-equal.sh
+	sleep 10
+	./set-audio-cam2.sh
+	sleep 10
+	./set-video-cam2.sh
+	./set-composite-fullscreen.sh
+	sleep 10
+	./set-video-cam1.sh
+	./set-audio-cam1.sh
+	sleep 10
+	./set-composite-side-by-side-equal.sh
+	sleep 10
+	./set-video-cam2.sh
+	sleep 10
+	./set-audio-cam2.sh
+	sleep 10
+	./set-video-cam1.sh
+	./set-audio-cam1.sh
+	./set-composite-fullscreen.sh
+done
