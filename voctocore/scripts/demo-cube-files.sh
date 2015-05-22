0dd95fb118d0863c4b8d1fe503020bb33950bc79
MaZderMind <github@mazdermind.de>
2015-05-22 12:25:56 +0200

    cycle modes on cube-demo

--- voctocore/scripts/demo-cube-files.sh
+++ voctocore/scripts/demo-cube-files.sh
@@ -4,11 +4,11 @@
 PID=$!
 echo "PID=$PID"
 sleep 1
-./set-composite-side-by-side-equal.sh
 ./av-source-cam1.sh &
 ./av-source-cam2.sh &
 ./av-record-output-ffmpeg-timestamps.sh &
 ./av-stream-hd.sh &
+./demo-cycle-modes.sh &
 while true; do sleep 1; done
 kill $PID
 wait
--- voctocore/scripts/demo-cube.sh
+++ voctocore/scripts/demo-cube.sh
@@ -4,11 +4,11 @@
 PID=$!
 echo "PID=$PID"
 sleep 1
-./set-composite-side-by-side-equal.sh
 ./av-source-bmd-cam1.sh &
 ./av-source-bmd-cam2.sh &
 ./av-record-output-ffmpeg-timestamps.sh &
 ./av-stream-hd.sh &
+./demo-cycle-modes.sh &
 while true; do sleep 1; done
 kill $PID
 wait
