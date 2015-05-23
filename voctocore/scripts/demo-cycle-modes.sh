1e493590167b2085904fbbaa68bd6046796a2581
MaZderMind <github@mazdermind.de>
2015-05-23 23:10:33 +0200

    better demo-cycle

--- voctocore/scripts/demo-cycle-modes.sh
+++ voctocore/scripts/demo-cycle-modes.sh
@@ -1,13 +1,15 @@
 while true; do
+	sleep 10
 	./set-composite-picture-in-picture.sh
 	sleep 10
 	./set-video-cam2.sh
+	./set-composite-fullscreen.sh
+	./set-audio-cam2.sh
 	sleep 10
 	./set-composite-side-by-side-preview.sh
+	./set-audio-cam1.sh
 	sleep 10
 	./set-video-cam1.sh
-	sleep 10
-	./set-composite-side-by-side-equal.sh
 	sleep 10
 	./set-audio-cam2.sh
 	sleep 10
