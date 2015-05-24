33fe27c28bc4aebe95655c91d584b2baf947da0b
MaZderMind <github@mazdermind.de>
2015-05-24 20:55:55 +0200

    change cycle, because it is soo nice to do that :)

--- voctocore/scripts/demo-cycle-modes.sh
+++ voctocore/scripts/demo-cycle-modes.sh
@@ -3,18 +3,17 @@ while true; do
 	./set-composite-picture-in-picture.sh
 	sleep 10
 	./set-video-cam2.sh
-	./set-composite-fullscreen.sh
 	./set-audio-cam2.sh
 	sleep 10
 	./set-composite-side-by-side-preview.sh
-	./set-audio-cam1.sh
 	sleep 10
+	./set-audio-cam1.sh
 	./set-video-cam1.sh
 	sleep 10
-	./set-audio-cam2.sh
-	sleep 10
-	./set-video-cam2.sh
 	./set-composite-fullscreen.sh
+	sleep 10
+	./set-audio-cam2.sh
+	./set-video-cam2.sh
 	sleep 10
 	./set-video-cam1.sh
 	./set-audio-cam1.sh
@@ -22,8 +21,9 @@ while true; do
 	./set-composite-side-by-side-equal.sh
 	sleep 10
 	./set-video-cam2.sh
-	sleep 10
 	./set-audio-cam2.sh
+	sleep 10
+	./set-composite-picture-in-picture.sh
 	sleep 10
 	./set-video-cam1.sh
 	./set-audio-cam1.sh
