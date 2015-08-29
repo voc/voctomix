9af81d19174cbd268066ee914581a8df09bd8a97
MaZderMind <peter@mazdermind.de>
2015-08-29 13:05:19 +0200

    don't mess with production room names

--- voctocore/scripts/av-stream-hd.sh
+++ voctocore/scripts/av-stream-hd.sh
@@ -9,4 +9,4 @@ ffmpeg -i tcp://localhost:11000 -aspect 16:9 -threads:0 0 \
 -ac 1 -c:a libfdk_aac -b:a 96k -ar 44100 \
 -map 0:a -filter:a:0 pan=mono:c0=FL \
 -ac:a:2 2 \
--y -f flv rtmp://127.0.0.1:1935/stream/s1_native_hd
+-y -f flv rtmp://127.0.0.1:1935/stream/voctomix
