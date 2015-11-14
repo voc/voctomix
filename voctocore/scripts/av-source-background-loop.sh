478b2e0e4510796b8f4a1e145e8e5c415b24e8b2
MaZderMind <git@mazdermind.de>
2015-11-14 17:09:51 +0100

    fix indentation in script

--- voctocore/scripts/av-source-background-loop.sh
+++ voctocore/scripts/av-source-background-loop.sh
@@ -1,6 +1,6 @@
 while true; do cat ~/bg.ts || exit 1; done |\
 	ffmpeg -re -i - \
 	-map 0:v \
-        -c:v rawvideo \
-        -f matroska \
-        tcp://localhost:16000
+	-c:v rawvideo \
+	-f matroska \
+	tcp://localhost:16000
