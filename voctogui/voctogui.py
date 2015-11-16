07f2ab39dc7acd278fa78ab841030207abe2e127
MaZderMind <git@mazdermind.de>
2015-11-17 00:24:26 +0100

    specify gi-version before importing from gi

--- voctogui/voctogui.py
+++ voctogui/voctogui.py
@@ -2,7 +2,10 @@
 import gi, signal, logging, sys, os
 
 # import GStreamer and GLib-Helper classes
+gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
+gi.require_version('GdkX11', '3.0')
+gi.require_version('GstVideo', '1.0')
 from gi.repository import Gtk, Gdk, Gst, GObject, GdkX11, GstVideo
 
 # check min-version
