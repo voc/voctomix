3579a052ae3ace1f67d816fd9bfdffc66c8168d4
MaZderMind <git@mazdermind.de>
2015-11-26 20:11:14 +0100

    [example scripts] use anullsrc instead of /div/zero which seems to be more robust

--- example-scripts/av-source-grabber.sh
+++ example-scripts/av-source-grabber.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 ffmpeg -y \
 	-i "http://10.73.5.2:1881/stream.mjpg" \
-	-filter_complex "scale=1920:1080,fps=25" \
-	-ar 48000 \
-	-ac 2 \
-	-f s16le \
-	-i "/dev/zero" \
+	-filter_complex " [0:v] scale=1920:1080,fps=25 [v] ; anullsrc=r=48000:cl=stereo [a] " \
+	-map "[v]" \
+	-map "[a]" \
 	-c:a pcm_s16le \
 	-c:v rawvideo \
 	-f matroska \
