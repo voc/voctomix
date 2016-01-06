38e1c2c0ed63db4053f198aac2c827141cf4fab8
MaZderMind <github@mazdermind.de>
2016-01-06 11:20:17 +0100

    don't rely on ffmpeg and external files for perf & benchmark

--- example-scripts/misc/benchmark.sh
+++ example-scripts/misc/benchmark.sh
@@ -5,8 +5,10 @@ PID=$!
 echo "PID=$PID"
 sleep 1
 ../control-server/set-composite-side-by-side-equal.sh >/dev/null 2>/dev/null
-../ffmpeg/source-testvideo-as-cam1.sh >/dev/null 2>/dev/null &
-../ffmpeg/source-testvideo-as-cam2.sh >/dev/null 2>/dev/null &
+../gstreamer/source-videotestsrc-as-cam1.sh >/dev/null 2>/dev/null &
+../gstreamer/source-videotestsrc-as-cam2.sh >/dev/null 2>/dev/null &
+../gstreamer/source-videotestsrc-as-grabber.sh >/dev/null 2>/dev/null &
+../gstreamer/source-videotestsrc-as-background-loop.sh >/dev/null 2>/dev/null &
 pidstat -p $PID 1 &
 sleep 10
 kill $PID
