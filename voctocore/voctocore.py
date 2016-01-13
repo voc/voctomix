77766845052fb3e88b40717988c77c1964cb4a90
Bjoern Riemer <bjoern.riemer@tu-berlin.de>
2016-01-13 12:37:02 +0100

    add --timestamp arg to voctocore

--- voctocore/voctocore.py
+++ voctocore/voctocore.py
@@ -56,8 +56,9 @@ class Voctocore(object):
 def main():
 	# configure logging
 	docolor = (Args.color == 'always') or (Args.color == 'auto' and sys.stderr.isatty())
+	timestamp = Args.timestamp > 0
 
-	handler = LogHandler(docolor)
+	handler = LogHandler(docolor,timestamp)
 	logging.root.addHandler(handler)
 
 	if Args.verbose >= 2:
