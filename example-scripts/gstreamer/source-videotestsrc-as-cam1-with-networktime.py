60e205cb92e7127742b42ff0e5c0bbc06df1d0e5
MaZderMind <git@mazdermind.de>
2016-01-28 11:21:52 +0100

    fix example resolution

--- example-scripts/gstreamer/source-videotestsrc-as-cam1-with-networktime.py
+++ example-scripts/gstreamer/source-videotestsrc-as-cam1-with-networktime.py
@@ -14,7 +14,7 @@ class Source(object):
 		pipeline = """
 			videotestsrc pattern=ball foreground-color=0x00ff0000 background-color=0x00440000 !
 				timeoverlay !
-				video/x-raw,format=I420,width=192,height=108,framerate=25/1,pixel-aspect-ratio=1/1 !
+				video/x-raw,format=I420,width=1920,height=1080,framerate=25/1,pixel-aspect-ratio=1/1 !
 				mux.
 
 			audiotestsrc freq=330 !
