140d4651afcdbf72a4b2637acd0c71925816eb82
Joel Addison <joel@addison.net.au>
2016-02-03 11:38:46 +1100

    Add keyboard shortcut to Cut button
    Use `t` to perform cut in video, as with dvswitch.

--- voctogui/lib/toolbar/misc.py
+++ voctogui/lib/toolbar/misc.py
@@ -4,11 +4,16 @@ from gi.repository import Gtk
 from lib.config import Config
 import lib.connection as Connection
 
+
 class MiscToolbarController(object):
 	""" Manages Accelerators and Clicks Misc buttons """
 
 	def __init__(self, drawing_area, win, uibuilder):
 		self.log = logging.getLogger('MiscToolbarController')
+
+		# Accelerators
+		accelerators = Gtk.AccelGroup()
+		win.add_accel_group(accelerators)
 
 		closebtn = uibuilder.find_widget_recursive(drawing_area, 'close')
 		closebtn.set_visible( Config.getboolean('misc', 'close') )
@@ -17,6 +22,9 @@ class MiscToolbarController(object):
 		cutbtn = uibuilder.find_widget_recursive(drawing_area, 'cut')
 		cutbtn.set_visible( Config.getboolean('misc', 'cut') )
 		cutbtn.connect('clicked', self.on_cutbtn_clicked)
+
+		key, mod = Gtk.accelerator_parse('t')
+		cutbtn.add_accelerator('clicked', accelerators, key, mod, Gtk.AccelFlags.VISIBLE)
 
 	def on_closebtn_clicked(self, btn):
 		self.log.info('close-button clicked')
