88ad03b7419f8ba64a6ee98a3d2d481b1d5b51e1
Florian Zeitz <florob@babelmonkeys.de>
2016-09-14 08:54:47 +0200

    generate-cut-list example: Ident by 4 spaces (PEP 8)

--- example-scripts/control-server/generate-cut-list.py
+++ example-scripts/control-server/generate-cut-list.py
@@ -6,16 +6,16 @@ import sys
 host = 'localhost'
 port = 9999
 
-conn = socket.create_connection( (host, port) )
+conn = socket.create_connection((host, port))
 fd = conn.makefile('rw')
 
 for line in fd:
-	words = line.rstrip('\n').split(' ')
+    words = line.rstrip('\n').split(' ')
 
-	signal = words[0]
-	args = words[1:]
+    signal = words[0]
+    args = words[1:]
 
-	if signal == 'message' and args[0] == 'cut':
-		ts = datetime.datetime.now().strftime("%Y-%m-%d/%H_%M_%S")
-		print(ts)
-		sys.stdout.flush()
+    if signal == 'message' and args[0] == 'cut':
+        ts = datetime.datetime.now().strftime("%Y-%m-%d/%H_%M_%S")
+        print(ts)
+        sys.stdout.flush()
