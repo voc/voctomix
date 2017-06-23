82572b32e5ddc8dd8eade591708eb2ab45ea2ae3
Florian Zeitz <florob@babelmonkeys.de>
2017-06-23 06:56:32 +0200

    voctocore: Rename NetTimeProvider6 to NetTimeProvider, to match exports

--- voctocore/lib/clock.py
+++ voctocore/lib/clock.py
@@ -12,4 +12,4 @@ Clock = Gst.SystemClock.obtain()
 log.info("Using System-Clock for all Pipelines: %s", Clock)
 
 log.info("Starting NetTimeProvider on Port %u", port)
-NetTimeProvider6 = GstNet.NetTimeProvider.new(Clock, '::', port)
+NetTimeProvider = GstNet.NetTimeProvider.new(Clock, '::', port)
