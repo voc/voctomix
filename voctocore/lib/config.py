61f25bba6e2ff1431c14dfc4fcc2efc5a834e229
Florian Zeitz <florob@babelmonkeys.de>
2017-06-23 06:28:14 +0200

    Subclass SafeConfigParser instead of monkey patching it

--- voctocore/lib/config.py
+++ voctocore/lib/config.py
@@ -6,11 +6,10 @@ from lib.args import Args
 __all__ = ['Config']
 
 
-def getlist(self, section, option):
-    return [x.strip() for x in self.get(section, option).split(',')]
+class VocConfigParser(SafeConfigParser):
+    def getlist(self, section, option):
+        return [x.strip() for x in self.get(section, option).split(',')]
 
-
-SafeConfigParser.getlist = getlist
 
 files = [
     os.path.join(os.path.dirname(os.path.realpath(__file__)),
@@ -27,7 +26,7 @@ files = [
 if Args.ini_file is not None:
     files.append(Args.ini_file)
 
-Config = SafeConfigParser()
+Config = VocConfigParser()
 readfiles = Config.read(files)
 
 log = logging.getLogger('ConfigParser')
--- voctogui/lib/config.py
+++ voctogui/lib/config.py
@@ -1,6 +1,6 @@
+import os.path
 import logging
 import json
-import os.path
 from configparser import SafeConfigParser
 from lib.args import Args
 import lib.connection as Connection
@@ -8,22 +8,18 @@ import lib.connection as Connection
 __all__ = ['Config']
 
 
-def getlist(self, section, option):
-    return [x.strip() for x in self.get(section, option).split(',')]
+class VocConfigParser(SafeConfigParser):
+    def getlist(self, section, option):
+        return [x.strip() for x in self.get(section, option).split(',')]
 
+    def fetchServerConfig(self):
+        log = logging.getLogger('Config')
+        log.info("reading server-config")
 
-def fetchServerConfig(self):
-    log = logging.getLogger('Config')
-    log.info("reading server-config")
+        server_config = Connection.fetchServerConfig()
 
-    server_config = Connection.fetchServerConfig()
-
-    log.info("merging server-config %s", server_config)
-    self.read_dict(server_config)
-
-
-SafeConfigParser.getlist = getlist
-SafeConfigParser.fetchServerConfig = fetchServerConfig
+        log.info("merging server-config %s", server_config)
+        self.read_dict(server_config)
 
 files = [
     os.path.join(os.path.dirname(os.path.realpath(__file__)),
@@ -38,5 +34,5 @@ files = [
 if Args.ini_file is not None:
     files.append(Args.ini_file)
 
-Config = SafeConfigParser()
+Config = VocConfigParser()
 Config.read(files)
