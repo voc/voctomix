3bdbf12ac6b38e3796e314d5b930170c9ea4c1ff
MaZderMind <git@mazdermind.de>
2017-08-09 08:53:38 +0200

    refactor voctogui config/args, too, to match voctocore structure and for future testability

--- voctogui/lib/args.py
+++ voctogui/lib/args.py
@@ -2,26 +2,33 @@ import argparse
 
 __all__ = ['Args']
 
-parser = argparse.ArgumentParser(description='Voctogui')
-parser.add_argument('-v', '--verbose', action='count', default=0,
-                    help="Also print INFO and DEBUG messages.")
+Args = None
 
-parser.add_argument('-c', '--color',
-                    action='store',
-                    choices=['auto', 'always', 'never'],
-                    default='auto',
-                    help="Control the use of colors in the Log-Output")
 
-parser.add_argument('-t', '--timestamp', action='store_true',
-                    help="Enable timestamps in the Log-Output")
+def parse():
+    global Args
 
-parser.add_argument('-i', '--ini-file', action='store',
-                    help="Load a custom config.ini-File")
+    parser = argparse.ArgumentParser(description='Voctogui')
+    parser.add_argument('-v', '--verbose', action='count', default=0,
+                        help="Also print INFO and DEBUG messages.")
 
-parser.add_argument('-u', '--ui-file', action='store',
-                    help="Load a custom .ui-File")
+    parser.add_argument('-c', '--color',
+                        action='store',
+                        choices=['auto', 'always', 'never'],
+                        default='auto',
+                        help="Control the use of colors in the Log-Output")
 
-parser.add_argument('-H', '--host', action='store',
-                    help="Connect to this host instead of the configured one.")
+    parser.add_argument('-t', '--timestamp', action='store_true',
+                        help="Enable timestamps in the Log-Output")
 
-Args = parser.parse_args()
+    parser.add_argument('-i', '--ini-file', action='store',
+                        help="Load a custom config.ini-File")
+
+    parser.add_argument('-u', '--ui-file', action='store',
+                        help="Load a custom .ui-File")
+
+    parser.add_argument('-H', '--host', action='store',
+                        help="Connect to this host "
+                             "instead of the configured one.")
+
+    Args = parser.parse_args()
--- voctogui/lib/config.py
+++ voctogui/lib/config.py
@@ -6,6 +6,8 @@ import lib.connection as Connection
 
 __all__ = ['Config']
 
+Config = None
+
 
 class VocConfigParser(SafeConfigParser):
     def getlist(self, section, option):
@@ -21,28 +23,37 @@ class VocConfigParser(SafeConfigParser):
         self.read_dict(server_config)
 
 
-files = [
-    os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                 '../default-config.ini'),
-    os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                 '../config.ini'),
-    '/etc/voctomix/voctogui.ini',
-    '/etc/voctogui.ini',
-    os.path.expanduser('~/.voctogui.ini'),
-]
+def load():
+    global Config
 
-if Args.ini_file is not None:
-    files.append(Args.ini_file)
+    files = [
+        os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                     '../default-config.ini'),
+        os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                     '../config.ini'),
+        '/etc/voctomix/voctogui.ini',
+        '/etc/voctogui.ini',
+        os.path.expanduser('~/.voctogui.ini'),
+    ]
 
-Config = VocConfigParser()
-readfiles = Config.read(files)
+    if Args.ini_file is not None:
+        files.append(Args.ini_file)
 
-log = logging.getLogger('ConfigParser')
-log.debug('considered config-files: \n%s',
-          "\n".join(["\t\t" + os.path.normpath(file) for file in files]))
-log.debug('successfully parsed config-files: \n%s',
-          "\n".join(["\t\t" + os.path.normpath(file) for file in readfiles]))
+    Config = VocConfigParser()
+    readfiles = Config.read(files)
 
-if Args.ini_file is not None and Args.ini_file not in readfiles:
-    raise RuntimeError('explicitly requested config-file "{}" '
-                       'could not be read'.format(Args.ini_file))
+    log = logging.getLogger('ConfigParser')
+    log.debug('considered config-files: \n%s',
+              "\n".join([
+                  "\t\t" + os.path.normpath(file)
+                  for file in files
+              ]))
+    log.debug('successfully parsed config-files: \n%s',
+              "\n".join([
+                  "\t\t" + os.path.normpath(file)
+                  for file in readfiles
+              ]))
+
+    if Args.ini_file is not None and Args.ini_file not in readfiles:
+        raise RuntimeError('explicitly requested config-file "{}" '
+                           'could not be read'.format(Args.ini_file))
