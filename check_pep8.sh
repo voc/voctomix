4d6252497459454278e8c7e42b416eca64af971f
MaZderMind <git@mazdermind.de>
2017-11-12 15:37:34 +0100

    ignore docs in pep8-checks

--- check_pep8.sh
+++ check_pep8.sh
@@ -2,7 +2,7 @@
 set -e
 
 # ignore import-not-at-top (required by gi)
-pycodestyle --ignore=E402 --exclude=voctocore/tests .
+pycodestyle --ignore=E402 --exclude=voctocore/tests,doc .
 
 # ignore long lines (prefer explanatory test-names)
 pycodestyle --ignore=E501 voctocore/tests
