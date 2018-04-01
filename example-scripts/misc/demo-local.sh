37d8088064199de4342e992720689f3002e9bc47
MaZderMind <git@mazdermind.de>
2018-04-01 12:37:46 +0200

    replace missing recortding-script in demo with segmented ffmpeg3 version

--- example-scripts/misc/demo-local.sh
+++ example-scripts/misc/demo-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-../../voctocore/voctocore.py &
+../../voctocore/voctocore.py -v &
 PID=$!
 echo "PID=$PID"
 sleep 1
@@ -8,6 +8,7 @@ sleep 1
 ../ffmpeg/source-testvideo-as-cam2.sh >/dev/null 2>/dev/null &
 ../ffmpeg/source-background-loop.sh &
 #../ffmpeg/stream-hd.sh &
+../ffmpeg/record-mixed-ffmpeg-segmented-timestamps.sh
 ../control-server/demo-cycle-modes.sh &
 ffplay tcp://localhost:11000
 kill $PID
