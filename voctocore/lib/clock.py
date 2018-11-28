c662d84e859984870c0dea67463bc5955a6119c3
Patrick Hoffmann <p.hoffmann@berlin.de>
2018-11-28 17:37:31 +0100

    voctocore: minor fix in log output of Clock

--- voctocore/lib/clock.py
+++ voctocore/lib/clock.py
@@ -9,7 +9,7 @@ log = logging.getLogger('Clock')
 
 log.debug("Obtaining System-Clock")
 Clock = Gst.SystemClock.obtain()
-log.info("Using System-Clock for all Pipelines: %s", Clock)
+log.info("Using System-Clock for all pipelines.")
 
 log.info("Starting NetTimeProvider on Port %u", port)
 NetTimeProvider = GstNet.NetTimeProvider.new(Clock, '::', port)
