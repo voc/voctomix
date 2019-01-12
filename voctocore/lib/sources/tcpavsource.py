f9b51c017a430bb0bf0c13d07c6680a088ce903b
Patrick Hoffmann <p.hoffmann@berlin.de>
2019-01-12 23:17:50 +0100

    voctocore: re-design of TCP Source (wip)
    todo: reconnect, blanking if not connected

--- voctocore/lib/sources/tcpavsource.py
+++ voctocore/lib/sources/tcpavsource.py
@@ -5,7 +5,6 @@ from gi.repository import Gst
 
 from lib.config import Config
 from lib.sources.avsource import AVSource
-from lib.tcpsingleconnection import TCPSingleConnection
 
 ALL_AUDIO_CAPS = Gst.Caps.from_string('audio/x-raw')
 ALL_VIDEO_CAPS = Gst.Caps.from_string('video/x-raw')
@@ -17,50 +16,53 @@ class TCPAVSource(AVSource):
         self.log = logging.getLogger('TCPAVSource[{}]'.format(name))
         AVSource.__init__(self, name, has_audio, has_video,
                           force_num_streams)
-        TCPSingleConnection.__init__(self, port)
+
+        deinterlacer = self.build_deinterlacer()
+        pipe = """
+tcpserversrc
+    do-timestamp=TRUE
+    port={port}
+    name=tcpsrc-{name}
+! matroskademux name=demux-{name}
+            """.format(
+            name=self.name,
+            port=port
+        )
+
+        if deinterlacer:
+            pipe += """
+demux-{name}.video_0
+! queue
+! video/x-raw
+! {deinterlacer}""".format(
+                name=self.name,
+                deinterlacer=deinterlacer
+            )
+        self.build_pipeline(pipe)
+        self.tcpsrc = None
+        self.audio_caps = Gst.Caps.from_string(Config.get('mix', 'audiocaps'))
+        self.video_caps = Gst.Caps.from_string(Config.get('mix', 'videocaps'))
+
+    def attach(self, pipeline):
+        super().attach(pipeline)
+        self.tcpsrc = pipeline.get_by_name(
+            'tcpsrc-{name}'.format(name=self.name))
+        demux = pipeline.get_by_name('demux-{name}'.format(name=self.name))
+        demux.connect('pad-added', self.on_pad_added)
 
     def __str__(self):
         return 'TCPAVSource[{name}] on tcp-port {port}'.format(
             name=self.name,
-            port=self.boundSocket.getsockname()[1]
+            port=self.tcpsrc.get_protperty(
+                "current-port") if self.tcpsrc else "<disconnected>"
         )
-
-    def on_accepted(self, conn, addr):
-        deinterlacer = self.build_deinterlacer()
-        pipeline = """
-fdsrc fd={fd} blocksize=1048576
-! queue
-! matroskademux name=demux""".format(
-            fd=conn.fileno()
-        )
-
-        if deinterlacer:
-            pipeline += """
-demux.
-! video/x-raw
-! {deinterlacer}""".format(
-                deinterlacer=self.build_deinterlacer()
-            )
-            self.build_pipeline(pipeline)
-
-        else:
-            self.build_pipeline(pipeline)
-
-        self.audio_caps = Gst.Caps.from_string(Config.get('mix', 'audiocaps'))
-        self.video_caps = Gst.Caps.from_string(Config.get('mix', 'videocaps'))
-
-        demux = self.pipeline.get_by_name('demux')
-        demux.connect('pad-added', self.on_pad_added)
-
-        self.pipeline.set_state(Gst.State.PLAYING)
 
     def build_deinterlacer(self):
         deinterlacer = super().build_deinterlacer()
-
-        if deinterlacer != '':
-            deinterlacer += ' name=deinter'
-
-        return deinterlacer
+        if deinterlacer:
+            return deinterlacer + ' name=deinter-{name}'.format(name=self.name)
+        else:
+            return None
 
     def on_pad_added(self, demux, src_pad):
         caps = src_pad.query_caps(None)
@@ -89,34 +91,23 @@ demux.
 
             self.test_and_warn_interlace_mode(caps)
 
-    def on_eos(self, bus, message):
-        super().on_eos(bus, message)
-        if self.currentConnection is not None:
-            self.disconnect()
-
-    def on_error(self, bus, message):
-        super().on_error(bus, message)
-        if self.currentConnection is not None:
-            self.disconnect()
-
-    def disconnect(self):
-        self.pipeline.set_state(Gst.State.NULL)
-        self.pipeline = None
-        self.close_connection()
-
     def restart(self):
-        if self.currentConnection is not None:
-            self.disconnect()
+        # TODO: reconnect
+        return
 
     def build_audioport(self, audiostream):
-        return 'demux.audio_{}'.format(audiostream)
+        return """
+demux-{name}.audio_{audiostream}
+! queue""".format(audiostream=audiostream, name=self.name)
 
     def build_videoport(self):
         deinterlacer = self.build_deinterlacer()
         if deinterlacer:
-            return 'deinter.'
+            return """
+deinter-{name}.""".format(name=self.name)
         else:
-            return 'demux.'
+            return """
+demux-{name}.""".format(name=self.name)
 
     def test_and_warn_interlace_mode(self, caps):
         interlace_mode = caps.get_structure(0).get_string('interlace-mode')
