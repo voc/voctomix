6b813dd5b9b85fe6b3525f849789618f7c670e25
Daniel Molkentin <daniel@molkentin.de>
2019-06-08 16:18:28 +0200

    Add optional overlay source
    The overlay source is a full screen source with the highest z-order
    within the compositor.

--- voctocore/lib/sources/__init__.py
+++ voctocore/lib/sources/__init__.py
@@ -6,6 +6,7 @@ from lib.sources.imgvsource import ImgVSource
 from lib.sources.tcpavsource import TCPAVSource
 from lib.sources.testsource import TestSource
 from lib.sources.videoloopsource import VideoLoopSource
+from lib.sources.overlaysource import OverlaySource
 
 log = logging.getLogger('AVSourceManager')
 
@@ -25,6 +26,8 @@ def spawn_source(name, port, has_audio=True, has_video=True,
         sources[name] = TestSource(name, has_audio, has_video)
     elif kind == 'videoloop':
         sources[name] = VideoLoopSource(name)
+    elif kind == 'overlay':
+        sources[name] = OverlaySource(name)
     elif kind == 'tcp':
         sources[name] = TCPAVSource(name, port, has_audio, has_video,
                                     force_num_streams)
