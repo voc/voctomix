8b704585110f3c0e4a3fd78c600e3719b9f47131
Patrick Hoffmann <p.hoffmann@berlin.de>
2019-07-18 12:53:10 +0200

    fixup: simplified DOT graphs for smaller PNG files

--- vocto/debug.py
+++ vocto/debug.py
@@ -12,6 +12,6 @@ def gst_generate_png(pipeline, name):
     dotfile = os.path.join(os.environ['GST_DEBUG_DUMP_DOT_DIR'], "%s.dot" % name)
     pngfile = os.path.join(os.getcwd(), "%s.dot.png" % name)
     log.debug("Generating PNG image of pipeline '{name}' into '{file}'".format(name=name, file=pngfile))
-    Gst.debug_bin_to_dot_file(pipeline, Gst.DebugGraphDetails.ALL, name)
+    Gst.debug_bin_to_dot_file(pipeline, 0, name)
     os.system("dot -Tpng -o{pngfile} {dotfile}".format(pngfile=pngfile,dotfile=dotfile))
     os.system("rm {dotfile}".format(pngfile=pngfile,dotfile=dotfile))
