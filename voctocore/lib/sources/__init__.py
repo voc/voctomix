6099451fe088fe74945bbeedeeee66896bd7ff3d
Patrick Hoffmann <p.hoffmann@berlin.de>
2019-11-11 22:22:07 +0100

    Use test sources as the default in configuration (and improve warning message, when falling back to)

--- voctocore/lib/sources/__init__.py
+++ voctocore/lib/sources/__init__.py
@@ -21,15 +21,16 @@ def spawn_source(name, port, has_audio=True, has_video=True,
         sources[name] = ImgVSource(name)
     elif kind == 'decklink':
         sources[name] = DeckLinkAVSource(name, has_audio, has_video)
-    elif kind == 'test':
-        sources[name] = TestSource(name, has_audio, has_video)
     elif kind == 'videoloop':
         sources[name] = VideoLoopSource(name)
     elif kind == 'tcp':
         sources[name] = TCPAVSource(name, port, has_audio, has_video,
                                     force_num_streams)
     else:
-        log.warning('Unknown source kind "%s", defaulting to "tcp"', kind)
+        if kind != 'test':
+            log.warning('Unknown value "%s" in attribute "kind" in definition of source %s (see section [source.%s] in configuration). Falling back to kind "test".', kind, name, name)
+        sources[name] = TestSource(name, has_audio, has_video)
+
     return sources[name]
 
 
