d437f5a2e0e316b236b5d27894a0557b5fc16203
Patrick Hoffmann <p.hoffmann@berlin.de>
2019-12-17 23:00:01 +0100

    added option -D (--gst-debug-details)
    Adjust the DOT graph pipeline description's detail as described in 
    https://gstreamer.freedesktop.org/documentation/gstreamer/gstdebugutils.html#GstDebugGraphDetails

--- voctogui/lib/args.py
+++ voctogui/lib/args.py
@@ -32,9 +32,12 @@ def parse():
                              "instead of the configured one.")
 
     parser.add_argument('-d', '--dot', action='store_true',
-                        help="Generate PNG files of DOT graphs of pipelines")
+                        help="Generate DOT files of pipelines into directory given in environment variable GST_DEBUG_DUMP_DOT_DIR")
 
-    parser.add_argument('-g', '--gstreamer-log', action='store_true',
-                        help="Log gstreamer messages into voctocore log")
+    parser.add_argument('-D', '--gst-debug-details', action='store', default=15,
+                        help="Set details in dot graph. GST_DEBUG_DETAILS must be a combination the following values: 1 = show caps-name on edges, 2 = show caps-details on edges, 4 = show modified parameters on elements, 8 = show element states, 16 = show full element parameter values even if they are very long. Default: 15 = show all the typical details that one might want (15=1+2+4+8)")
+
+    parser.add_argument('-g', '--gstreamer-log', action='count', default=0,
+                        help="Log gstreamer messages into voctocore log (Set log level by using -g, -gg or -ggg).")
 
     Args = parser.parse_args()
